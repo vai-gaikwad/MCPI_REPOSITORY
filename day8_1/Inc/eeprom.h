33a12f2e7f389118b8ae1b761cfb07f401e7a457
vai-desd <gaikwadvd96@gmail.com>
2024-04-02 23:50:23 +0530

    day 8 assignment

---
+++ day8_1/Inc/eeprom.h
@@ -0,0 +1,20 @@
+/*
+ * eeprom.h
+ *
+ *  Created on: Apr 2, 2024
+ *      Author: vai
+ */
+
+#ifndef EEPROM_H_
+#define EEPROM_H_
+
+#include "i2c.h"
+
+#define EEPROM_W	0xA0
+#define EEPROM_R	0xA1
+
+void EEPROM_Init(void);
+void EEPROM_Write(uint16_t addr, uint8_t byte[], uint8_t n);
+void EEPROM_Read(uint16_t addr, uint8_t byte[], uint8_t n);
+
+#endif /* EEPROM_H_ */
